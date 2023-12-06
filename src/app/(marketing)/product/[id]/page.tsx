5d9e5f7a6d046629ff2748c5f0193974130caf12
Unkeloc4 <138797225+Unkeloc4@users.noreply.github.com>
2023-12-06 09:01:14 +0700

    update

--- src/app/(marketing)/product/[id]/page.tsx
+++ src/app/(marketing)/product/[id]/page.tsx
@@ -1,8 +1,21 @@
+"use client"
+import { useSession} from "next-auth/react";
+import { NotLoginPage } from "@/app/(marketing)/_components/screen/not_login";
 const ProductView = ({
     params: { id },
   }: {
     params: { id: string }
   }) => {
-    return <div>{id}</div>
-}
+
+
+    const { data: session} = useSession();
+
+    if(session){
+      return <div>{id}</div>
+    }
+
+    return <NotLoginPage />
+  }
+
+
 export default ProductView;
